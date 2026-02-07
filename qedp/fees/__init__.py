819546abd2d56c9ca33931833c2ca24ab6860aa2
joelyanglq <joelyangqq@outlook.com>
2026-02-06 23:42:44 -0500

    remove qedp

--- qedp/fees/__init__.py
+++
@@ -1,9 +0,0 @@
-"""qedp.fees package."""
-from qedp.fees.cost_models import SlippageModel, LinearImpactSlippage, FixedBpsSlippage, FeeModel
-
-__all__ = [
-    "SlippageModel",
-    "LinearImpactSlippage",
-    "FixedBpsSlippage",
-    "FeeModel",
-]
